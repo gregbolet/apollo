315b6e2628193d93884514aa200baaa228b212cc
gregboleet <gbolet@vt.edu>
2021-06-16 12:06:51 -0700

    added a build script to make building easier

--- runBuild.sh
+++ runBuild.sh
@@ -2,7 +2,7 @@
 
 cd ./build
 cmake   -DENABLE_MPI=OFF \
-	-OpenCV_DIR=~/workspace/opencv/build \
+	-DOpenCV_DIR=~/workspace/opencv/build \
 	-DENABLE_PERF_CNTRS=ON \
 	-DPAPI_DIR=/usr/WS2/bolet1/spack/opt/spack/linux-rhel7-broadwell/gcc-10.2.1/papi-6.0.0.1-42m6tpjegu2j7hxrz26ii5t23jcmtmca \
 	../
