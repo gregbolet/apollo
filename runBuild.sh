e004f111f07f8f4ea9ee613a454fd98b09a2bf2c
gregboleet <gbolet@vt.edu>
2021-08-05 07:04:09 -0700

    backup

--- runBuild.sh
+++ runBuild.sh
@@ -13,7 +13,7 @@ cmake   -DENABLE_MPI=OFF \
  	-DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=./install \
 	-DOpenCV_DIR=~/workspace/opencv/build \
-	-DENABLE_PERF_CNTRS=ON \
+	-DENABLE_PERF_CNTRS=OFF \
 	-DPAPI_DIR=/usr/WS2/bolet1/spack/opt/spack/linux-rhel7-broadwell/gcc-10.2.1/papi-6.0.0.1-42m6tpjegu2j7hxrz26ii5t23jcmtmca \
 	../
 
